367f2e030e6b2572ab601c95708f194471ca25a6
MinHyungRHIE <quotia72@naver.com>
2020-02-23 00:49:52 +0900

    cafePosProgram

--- src/_99_practice/Example.java
+++ src/_99_practice/Example.java
@@ -4,23 +4,20 @@ import java.util.Scanner;
 
 public class Example {
 
-	public static void A(int[] ticketNum) {
-		ticketNum[0] +=1;
-		return;
-	}
-
-	public static void B(int[] tn) {
-		tn[1] +=2;
-	}
-
-
 	public static void main(String[] args) {
-
-		//DB
-		int[] ticketNum = {10,20,14,55};
+		
 		Scanner sc = new Scanner(System.in);
-		A(ticketNum);
-		B(ticketNum);
+		boolean isEnd = false;
+		
+		while(!isEnd) {
+			System.out.println("입력하세요");
+			int num = sc.nextInt();
+			if(num ==0) {
+				isEnd=true;
+			}
+		}
+		System.out.println("프로그램 종료");
 	}
+	
 }
 
