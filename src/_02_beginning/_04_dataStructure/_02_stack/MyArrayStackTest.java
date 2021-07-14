adf14ddac10b34e724985f55ecfe18303b15be43
dlalstj0213 <dlalstj0213@gmail.com>
2021-07-14 17:41:28 +0900

    MyArrayStack

---
+++ src/_02_beginning/_04_dataStructure/_02_stack/MyArrayStackTest.java
@@ -0,0 +1,21 @@
+package _02_beginning._04_dataStructure._02_stack;
+
+public class MyArrayStackTest {
+	
+	public static void main(String[] args) {
+
+		MyArrayStack stack = new MyArrayStack(3);
+		
+		stack.push(10);
+		stack.push(20);
+		stack.push(30);
+		stack.push(40);
+		
+		stack.printAll();
+		
+		System.out.println("top element is " + stack.pop());
+		stack.printAll();
+		System.out.println("stack size is " + stack.getSize());
+	}
+
+}
