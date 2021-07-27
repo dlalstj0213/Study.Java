f050a6ca17f8981a4c7bbbe09fb98137d73c7d6c
dlalstj0213 <dlalstj0213@gmail.com>
2021-07-27 17:01:20 +0900

    functional interface

---
+++ src/_03_intermediate/_02_functional_interface/MyNumberTest.java
@@ -0,0 +1,10 @@
+package _03_intermediate._02_functional_interface;
+
+public class MyNumberTest {
+	public static void main(String[] args) {
+		MyNumber myNumber = (x, y) -> x > y? x : y;
+		
+		System.out.println(myNumber.getMax(10, 20));
+		
+	}
+}
